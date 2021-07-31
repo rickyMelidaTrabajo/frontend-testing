af7500b4f6291546bbd3900ad7f8a44b61cd5fd1
RickyMelidaTrabajo <ricardomelida92.trabajo@gmail.com>
2021-07-31 04:31:12 -0400

    Se completa ejercicio 4

--- exercises/exer-4.js
+++ exercises/exer-4.js
@@ -14,5 +14,22 @@ const { createAll } = require('../helpers/data.helper');
 
 const companies = createAll();
 
+const userTable = (companies) => {
+    let users = [];
+    companies.forEach(company => {
+        company.users.forEach(usrs => {
+            usrs.company = company.name;
+            users.push(usrs);
+        })
+    });
+
+    const orderedUsers = users.sort((user1, user2) => user2.age - user1.age)
+
+    console.table(orderedUsers)
+}
+
+
+//userTable(companies)
+
 cleanConsole(4, companies);
 console.log('%c ---- RES 4 --- ', 'background: #bada55; color: #222', 'Put here your method: ');
